f40d432253d813990209946ec5d6c26250712761
DavidRegino <124410591+DavidRegino@users.noreply.github.com>
2023-09-12 19:30:05 -0700

    Add files via upload

--- parcial1/pares.js
+++ parcial1/pares.js
@@ -2,9 +2,8 @@ $(document).ready(function(){
     var carta1 = ""; var carta2 = "";
     var par= false;
     var total_pares = 0; 
-    var intentos = 0;
     var pares_restantes = 8;
-    var timer = 5;
+    var total_fallas =0;
 
     $('img').click(function(e){
         var estado = $(this).attr('data-estado')
@@ -20,14 +19,11 @@ $(document).ready(function(){
                     carta2.attr('src','imagenes/carta volteada.jpg');
                     carta1.attr('data-estado', '0')
                     carta2.attr('data-estado', '0')
-                    intentos++
-                    $("#intentos").html(intentos)
+                    total_fallas++
                 }else{
                     console.log("Es par")
                     carta1.attr('data-estado', '1')
                     carta2.attr('data-estado', '1')
-                    intentos++
-                    $("#intentos").html(intentos)
                     par=false
                 }
                 carta1=""
@@ -51,17 +47,42 @@ $(document).ready(function(){
         if(carta1.attr('data-id') == carta2.attr('data-id')){
             par = true
             total_pares++
-            intentos++
-            pares_restantes--
-            $("#intentos").html(intentos)
             $("#total_pares").html(total_pares)
             carta1 = ""
             carta2 = "" 
-        }
-        if(pares_restantes == 0){
-                alert("GANASTE WAOS ERES UN CRACK COMO FERNANFLOO, la pagina se reiniciara en 5 segundos al cerrar esta alerta")
-                setInterval("location.reload()",5000);
-            }  
+        }  
+    }
+
+    if(total_pares >=8){
+        alert("ganaste")
+        //reiniciando la pagina setInterval("location.reload()");
+        $('img').each(function (){
+            $(this).attr('src','imagenes/carta volteada.jpg')
+            $(this).attr('data-estado', '0')
+        })
+
+        total_pares=0
+        total_fallas=0
+        par=false
+        carta1=""
+        carta2=""
+        return;
+    }
+    if(total_fallas >=5){
+        alert("has alcansado el limite de intentos")
+        alert("perdiste")
+        //reiniciando la pagina setInterval("location.reload()");
+        $('img').each(function (){
+            $(this).attr('src','imagenes/carta volteada.jpg')
+            $(this).attr('data-estado', '0')
+        })
+
+        total_pares=0
+        total_fallas=0
+        par=false
+        carta1=""
+        carta2=""
+        return;
     }
     
     });
